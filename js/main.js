9f0f79b4245b8053c321ac916cd7ccd328f76192
mlaska2 <60201530+mlaska2@users.noreply.github.com>
2020-03-22 13:52:01 -0500

    lesson 2 activity 8 complete

--- js/main.js
+++ js/main.js
@@ -27,4 +27,70 @@ window.onload = function() {
         .attr("x", 50) //position from the left on the x axis
         .attr("y", 50) //position from the top on the y axis (because top left is origin)
         .style("fill", "#FFFFFF"); //fill color = white
+
+    // //create data array
+    // //var dataArray = [10,20,30,40,50];
+    //
+    // //block to create multiple, different circles at once using 3 methods supporting a join d3.selectAll, .data(), and .enter()
+    // //need to use d3.selectAll instead of d3.select bc that only makes use of a single datum
+    // var circles = container.selectAll(".circles") //selectAll selects all matching elements in the DOM - feeding parameter that doesnt return anything creates an empty selection
+    //     .data(dataArray) // feed in a data array to the data() method
+    //     .enter() // one of the great mysteries of the universe...?
+    //     //now functions like a loop through the data array - each operator applied once for each value in data array
+    //     .append("circle") //add a circle for each datum in dataArray
+    //     .attr("class", "circles") //apply class name to all circles
+    //     .attr("r", function(d, i) { //can make use of datum and index in array with 2 parameters in anonymous funciton
+    //         console.log("d:", d, "i:", i) //looking at d and i
+    //         return d;
+    //     })
+    //     .attr("cx", function(d, i) {
+    //         return 70 + (i*180);
+    //     })
+    //     .attr("cy", function(d, i) {
+    //         return 450 - (d*5);
+    //     });
+
+    //create data array of cities
+    var cityPop = [
+        {
+            city: 'Madison',
+            population: 233209
+        },
+        {
+            city: 'Milwaukee',
+            population: 594833
+        },
+        {
+            city: 'Green Bay',
+            population: 104057
+        },
+        {
+            city: 'Superior',
+            population: 27244
+        }
+    ];
+
+    //block to create multiple, different circles at once using 3 methods supporting a join d3.selectAll, .data(), and .enter()
+    var circles = container.selectAll(".circles") //create an empty selection
+        .data(cityPop) //feed in the array of data
+        .enter() //mystery
+        //loop starts here doing though each operator once for each value in dataArray
+        .append("circle") //expect HTML --WOW! some circles there
+        .attr("class", "circles") //apply class name to circles
+        .attr("id", function(d) { //d still holds each array value, but that value is an object w/ 2 properties (city and population)
+            return d.city;
+        })
+        .attr("r", function(d) {
+            //calculate the radius based on pop value as circle area
+            var area = d.population * .01;
+            return Math.sqrt(area/Math.PI);
+        })
+        .attr("cx", function(d, i) {
+            //use index to place circles horizontally
+            return 90 + (i*180);
+        })
+        .attr("cy", function(d) {
+            //subtract value from 450 to "grow" circles up from bottom instead of down from top of svg
+            return 450 - (d.population*0.0005);
+        })
 };
