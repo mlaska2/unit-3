57d84911c55c63980776e673c28503f75c4480f4
mlaska2 <60201530+mlaska2@users.noreply.github.com>
2020-03-29 22:15:05 -0500

    activity 10 lesson 1 step 2 complete
    moving attribute array and expressed variables to the top of main.js, encapsulating script within a self-executing anonymous wrapper function, and grouping tasks within the callback into their own defined functions

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 // Script by Matthew Laska; Unit 3 - D3 Lab; G575 Spring 2020
+
+(function() {
+
+//pseudo-global variables
+    //variables for data join
+var attrArray = ["Volume Index of GDP/Capita", "Unemployment Rate", "Life Expectancy", "GHG Emissions/Capita", "National Debt as % of GDP"];
+var expressedAttr = attrArray[0] //initial attribute
+
+//begin script when window loads
 window.onload = setMap();
 
 //set up choropleth map
@@ -46,59 +55,16 @@ function setMap() {
         // console.log(world);
         // console.log(eUnion);
 
-        //create graticule generator
-        var graticule = d3.geoGraticule()
-            .step([10, 10]); //place graticule lines every 5 degrees of [lon,lat]
-
-        //create graticule background
-        var gratBackground = laskaMap.append("path")
-            .datum(graticule.outline()) //bind graticule background
-            .attr("class", "gratBackground")
-            .attr("d", path) //project graticule background
-
-        //create graticule lines
-        var gratLines = laskaMap.selectAll(".gratLines") //select graticule elements that will be created
-            .data(graticule.lines()) //bind graticule lines to each element to be created
-            .enter() //create an element for each datum
-            .append("path") //append each element (grat line) to the svg as a path element
-            .attr("class", "gratLines") //assign class for styling
-            .attr("d", path); //project graticule lines by putting in path generator
+        //place graticule on map
+        setGraticule(laskaMap, path);
 
         //translate world and eUnion topoJSON to geoJSON FeatureCollections
         var worldCountries = topojson.feature(world, world.objects.ne_50m_admin_0_countries),
             euCountries = topojson.feature(eUnion, eUnion.objects.euCountries).features; //add on .features because .data needs array as parameter, but topojson.feature turns topojson object to geojson FeatureCollection object. thus, for europeanUnion block to work, need to pull array of features out of FeatureCollection and pass it to .data()
 
         //examine results
-        // console.log(worldCountries);
+        //console.log(worldCountries);
         //console.log(euCountries)
-
-//start week 10 stuff
-        //variables for data join
-        var attrArray = ["Volume Index of GDP/Capita", "Unemployment Rate", "Life Expectancy", "GHG Emissions/Capita", "National Debt as % of GDP"];
-
-        //loop through csv to assign each set of csv attribute values to geojson region
-        for (var i=0; i<csvData.length; i++) {
-            var csvCountry = csvData[i]; //index of the current country
-            var csvKey = csvCountry.Country; //the csv primary key to be used for join
-
-            //loop through geojson countries to find correct country
-            for (var a=0; a<euCountries.length; a++) {
-
-                var geojsonProperties = euCountries[a].properties //the current countries geojson properties
-                var geojsonKey = geojsonProperties.Country //the geojson primary key to be used for the join, should be the same as csvKey
-
-                //where primary keys match, transfer csv data to geojson properties object
-                if (geojsonKey == csvKey) {
-                    attrArray.forEach(function(attr){
-                        var value = parseFloat(csvCountry[attr]); //get csv attribute value
-                        geojsonProperties[attr] = value; //assign attribute and value to geojson properties
-                    });
-                };
-            };
-        };
-        //console.log(euCountries)
-
-//end week 10 stuff
 
         //add world countries to map
         var countries = laskaMap.append("path")
@@ -106,14 +72,76 @@ function setMap() {
             .attr("class", "countries")
             .attr("d", path)
 
-        //add EU Countries to map - create enumeration units for choropleth. use select all,data,enter to draw each feature corresponding to a country separately
-        var europeanUnion = laskaMap.selectAll(".europeanUnion")
-            .data(euCountries)
-            .enter()
-            .append("path")
-            .attr("class", function(d) {
-                return "europeanUnion " + d.properties.Country;
-            })
-            .attr("d", path)
+        //join csv data to GeoJSON enumeration units
+        euCountries = joinData(euCountries, csvData);
+
+        //add enumeration units to the map
+        setEnumerationUnits(euCountries, laskaMap, path);
     };
+}; //end of setMap function
+
+//function to create graticule and background
+function setGraticule(laskaMap, path) {
+
+    //create graticule generator
+    var graticule = d3.geoGraticule()
+        .step([10, 10]); //place graticule lines every 5 degrees of [lon,lat]
+
+    //create graticule background
+    var gratBackground = laskaMap.append("path")
+        .datum(graticule.outline()) //bind graticule background
+        .attr("class", "gratBackground")
+        .attr("d", path) //project graticule background
+
+    //create graticule lines
+    var gratLines = laskaMap.selectAll(".gratLines") //select graticule elements that will be created
+        .data(graticule.lines()) //bind graticule lines to each element to be created
+        .enter() //create an element for each datum
+        .append("path") //append each element (grat line) to the svg as a path element
+        .attr("class", "gratLines") //assign class for styling
+        .attr("d", path); //project graticule lines by putting in path generator
 };
+
+function joinData(euCountries, csvData) {
+
+    //loop through csv to assign each set of csv attribute values to geojson region
+    for (var i=0; i<csvData.length; i++) {
+        var csvCountry = csvData[i]; //index of the current country
+        var csvKey = csvCountry.Country; //the csv primary key to be used for join
+
+        //loop through geojson countries to find correct country
+        for (var a=0; a<euCountries.length; a++) {
+
+            var geojsonProperties = euCountries[a].properties //the current countries geojson properties
+            var geojsonKey = geojsonProperties.Country //the geojson primary key to be used for the join, should be the same as csvKey
+
+            //where primary keys match, transfer csv data to geojson properties object
+            if (geojsonKey == csvKey) {
+                attrArray.forEach(function(attr){
+                    var value = parseFloat(csvCountry[attr]); //get csv attribute value
+                    geojsonProperties[attr] = value; //assign attribute and value to geojson properties
+                });
+            };
+        };
+    };
+
+    console.log(euCountries);
+
+    return euCountries;
+
+};
+
+function setEnumerationUnits(euCountries, laskaMap, path) {
+
+    //add EU Countries to map - create enumeration units for choropleth. use select all,data,enter to draw each feature corresponding to a country separately
+    var europeanUnion = laskaMap.selectAll(".europeanUnion")
+        .data(euCountries)
+        .enter()
+        .append("path")
+        .attr("class", function(d) {
+            return "europeanUnion " + d.properties.Country;
+        })
+        .attr("d", path)
+};
+
+})(); //last line of main.js, call the self-executing anonymous function
